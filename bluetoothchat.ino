2e3ea9eebde6822accd61aa0a318b2e6ad743b1c
fvandal <fvandal@udel.edu>
2017-04-04 18:45:10 -0400

    bluetooth communicating code
    code for communicating with bluetooth module HC-05 with arduino to computer's bluetooth module

---
+++ bluetoothchat.ino
@@ -0,0 +1,64 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial portOne(3, 2); // RX, TX
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.println("Goodnight moon!");
+
+  // set the data rate for the SoftwareSerial port
+  delay(10);
+  portOne.begin(9600);
+
+  portOne.println("hc-05");
+  // set the data rate for the SoftwareSerial port
+  pinMode(13, OUTPUT); 
+
+}
+char serial = 0;
+char port = 0;
+int ifcount = 0;
+void loop() { // run over and over
+    portOne.listen();
+
+    if (portOne.available()) {
+      while(portOne.available()){
+      port = portOne.read();
+      digitalWrite(13, HIGH);
+      portOne.write(port);
+//      portOne.println();
+
+      Serial.write(port);
+//      Serial.println();
+
+      }
+            portOne.println();
+            Serial.println();
+
+
+
+    }
+  
+
+  if (Serial.available()) {
+    while(Serial.available()){
+    serial = Serial.read();
+    digitalWrite(13, LOW);
+
+    
+    Serial.write(serial);
+//Serial.println();
+portOne.write(serial);
+//portOne.println();
+ }
+ portOne.println();
+            Serial.println();
+
+  }
+}
